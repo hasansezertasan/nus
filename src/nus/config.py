92715571bd78607755ac763ed8ccd5dba1d8cf7a
hasansezertasan <hasansezertasan@gmail.com>
2025-08-05 02:14:22 +0300

    ref(logging): ensure log file directory is created only once
    - Moved directory creation for the log file into the logger setup function to prevent redundancy.
    - Enhanced docstring for clarity on logger configuration and attributes.
    - Ensured log handlers are added only once to avoid duplicate entries.

--- src/nus/config.py
+++ src/nus/config.py
@@ -6,7 +6,5 @@ ROOT_FOLDER_NAME = ".nus"
 DEFAULT_USER_HOME_CACHE_PATH = Path.home() / ROOT_FOLDER_NAME
 """Path to the user home cache path."""
 
-DEFAULT_USER_HOME_CACHE_PATH.mkdir(parents=True, exist_ok=True)
-
 log_file_path: Path = DEFAULT_USER_HOME_CACHE_PATH / "nus.log"
 """Path to the log file."""
